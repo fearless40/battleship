dbd1206881fcb259826463c3ea4646fa6761169e
Fearless40 <spivack@gmail.com>
2025-11-11 11:25:40 -0800

    Writing the game server in the header. Need to finish it off.

--- include/gameserver/server.hpp
+++ include/gameserver/server.hpp
@@ -4,24 +4,93 @@
 #include "gamelayout.hpp"
 #include "rowcol.hpp"
 #include "ship.hpp"
+#include <algorithm>
 #include <array>
 #include <bitset>
+#include <compare>
+#include <cstddef>
+#include <iterator>
+#include <numeric>
 #include <optional>
+#include <span>
 
 inline namespace v1 {
 
-class ClassicPlayer {
-public:
-private:
-public:
-  // Returns true if shot is allowed
-  bool fire_single_shot(RowCol value);
-
-  Response last_guess_result() const noexcept;
+namespace detail {
+template <class Tag> struct TypedID {
+  int value{0};
+  std::strong_ordering operator<=>(const TypedID<Tag> &other) const = default;
 };
+struct GameID {};
+struct UserID {};
+
+} // namespace detail
+using ID = detail::TypedID<detail::GameID>;
+using UserID = detail::TypedID<detail::UserID>;
 
 class ClassicGameServer {
+public:
   using history = std::pair<RowCol, Response>;
+  class ClientPlayer {
+    ClassicGameServer &mServer;
+    ID mId;
+    UserID mUser_id;
+
+  public:
+    struct ShipStatus {
+      ShipDefinition id{0};
+      std::size_t number_hits{0};
+      bool sunk{false};
+    };
+
+  public:
+    ClientPlayer(ClassicGameServer &serv, ID playerid, UserID user_id)
+        : mServer(serv), mId(playerid), mUser_id(user_id) {};
+
+    bool fire_single_shot(RowCol guess) { return false; }
+
+    Response last_response() const noexcept {
+      const auto player = mServer.get_player(mId);
+      return player.shots.back().second;
+    }
+
+    const std::span<const history> prior_shots() const noexcept {
+      const auto &player = mServer.get_player(mId);
+      return {player.shots};
+    }
+
+    const int ship_hits(ShipDefinition ship) const noexcept {
+      const auto &player = mServer.get_player(mId);
+      auto it = std::ranges::find(player.hits, ship, &ShipHit::id);
+      if (it == player.hits.end())
+        return 0;
+
+      return it->hits.count();
+    }
+
+    const std::vector<ShipStatus> ships_all_hits() const {
+
+      std::vector<ShipStatus> results;
+      results.reserve(mServer.number_ships());
+
+      auto const &p = mServer.get_player(mId);
+      std::transform(p.hits.begin(), p.hits.end(), std::back_inserter(results),
+                     [](auto const &hit) -> ShipStatus {
+                       return {hit.id, hit.hits.count(),
+                               hit.hits.count() == hit.id.size};
+                     });
+
+      return results;
+    };
+
+    constexpr auto id() const noexcept { return mId; }
+
+    constexpr auto user_id() const noexcept { return mUser_id; }
+  };
+
+  friend class ClientPlayer;
+
+private:
   struct ShipHit {
     std::bitset<32> hits;
     ShipDefinition id;
@@ -29,7 +98,8 @@ class ClassicGameServer {
   };
 
   struct Player {
-    int user_id{0};
+    ID id;
+    UserID user_id;
     std::vector<ShipHit> hits;
     Ship::Ships ships;
     std::vector<history> shots;
@@ -46,16 +116,39 @@ private:
 
 private:
   // Private methods
+  constexpr std::size_t number_ships() const noexcept {
+    return mLayout.maxShipSize.size - mLayout.minShipSize.size;
+  }
+
+  bool did_player_lose(ID player_id) const noexcept {
+    auto const &p = mPlayers[player_id.value];
+    auto count_sunk =
+        std::count_if(p.hits.begin(), p.hits.end(),
+                      [](const ShipHit &hit) { return hit.is_sunk(); });
+    return count_sunk == number_ships();
+  }
+
+  bool is_game_over() const noexcept {
+    for (auto &p : mPlayers) {
+      if (did_player_lose(p.id) == true)
+        return true;
+      ;
+    }
+    return false;
+  }
+
+  const Player &get_player(ID id) const { return mPlayers[id.value]; }
 
 public:
   // Methods
   ClassicGameServer(const GameLayout &layout) : mLayout(layout) {}
 
-  std::optional<ClassicPlayer> set_player(int user_id, Ship::Ships &&ships) {
+  std::optional<ClientPlayer> set_player(UserID user_id, Ship::Ships &&ships) {
     if (mGame_started)
       return {};
 
     auto &player = mPlayers[mCurrent_player];
+    player.id = ID(mCurrent_player);
     player.ships = std::move(ships);
     player.user_id = user_id;
     player.hits.reserve(player.ships.size());
@@ -64,11 +157,42 @@ public:
       player.hits.emplace_back(std::bitset<32>{}, s.id());
     }
 
-    void set_first_player(const ClassicPlayer &player);
+    ++mCurrent_player;
+    return ClientPlayer(*this, player.id, player.user_id);
+  }
 
-    bool game_finished() const noexcept;
+  void set_first_player(const ClientPlayer &player) {
+    if (mGame_started)
+      return;
 
-    std::size_t current_round();
-    bool next_round(); // Returns true if game is not won;
-  };
+    mCurrent_player = player.id().value;
+  }
+
+  bool game_finished() const noexcept { return mGame_won; }
+
+  std::size_t current_round() { return mRound_count; }
+  bool next_round() {
+    if (!mGame_started) {
+      mGame_started = true;
+      mGame_won = false;
+      mRound_count = 0;
+      return true;
+    }
+
+    ++mRound_count;
+    ++mCurrent_player;
+    if (mCurrent_player > 1)
+      mCurrent_player = 0;
+
+    if (is_game_over()) {
+      mGame_won = true;
+      mGame_started = false;
+      return false;
+    }
+
+    return true;
+  }
+
+  // Returns true if game is not won;
+};
 } // namespace v1
