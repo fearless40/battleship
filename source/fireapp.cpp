3c45eb4e766068d11c8e7ab2bbc085c70597d02a
Fearless40 <spivack@gmail.com>
2026-02-12 17:30:06 -0800

    Fixing some issues.

--- source/fireapp.cpp
+++ source/fireapp.cpp
@@ -11,8 +11,13 @@
 #include <render.hpp>
 #include <term_control.hpp>
 
-term::Image<term::pixel::Pixel<term::pixel::BgColor>> bgColor{
-    term::compositor::Width{30}, term::compositor::Height{30}};
+namespace pixel {
+using namespace term::pixel;
+using Pix = Pixel<pixel::ASCII, pixel::Color>;
+} // namespace pixel
+
+term::Image<pixel::Pix> color{term::compositor::Width{30},
+                              term::compositor::Height{30}};
 
 int offset{0};
 unsigned char buffer[8096];
@@ -23,25 +28,31 @@ void render_frame() {
 
   auto output = std::span<unsigned char>(buffer, 8096);
 
-  for (auto row : bgColor.rows()) {
+  for (auto row : color.rows()) {
     for (auto [col, pixel] : std::views::enumerate(row)) {
-      pixel.bgblue = (blue + step * col) + offset;
-      pixel.bggreen = 0;
-      pixel.bgred = 0;
+      pixel.blue = (blue + step * col) + offset;
+      pixel.green = pixel.blue;
+      pixel.red = pixel.blue;
     }
   }
 
-  term::details::render_to_buffer(bgColor, output);
+  offset++;
+  offset = std::clamp(offset, 0, 200);
+  term::details::render_to_buffer(color, output);
 
   std::cout << buffer;
   std::cout << "\e[H";
 };
 
 int main(int argv, char *arguments[]) {
-
-  constexpr const float milli_per_frame = 1000.f / 60.f;
+  using namespace std::chrono_literals;
+  constexpr const std::chrono::milliseconds milli_per_frame = 1000ms / 20;
   auto start_time = std::chrono::steady_clock::now();
   bool loop = true;
+
+  for (auto row : color.rows()) {
+    std::ranges::fill(row, pixel::Pix{'X', {0, 0, 0}});
+  }
 
   std::println("Starting...");
 
@@ -52,8 +63,9 @@ int main(int argv, char *arguments[]) {
     if (std::chrono::milliseconds frame_time =
             std::chrono::duration_cast<std::chrono::milliseconds>(next_time -
                                                                   start_time);
-        frame_time.count() > milli_per_frame) {
+        frame_time > milli_per_frame) {
       render_frame();
+      start_time = std::chrono::steady_clock::now();
     }
 
     if (term.had_key_event()) {
